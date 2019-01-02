785de2ffbfac32cb0c4b6def77eb05b548d901ea
KaMeHb-UA <marlock@etlgr.com>
2019-01-02 19:37:53 +0200

    `install` command done

--- install.js
+++ install.js
@@ -1,10 +1,28 @@
 const { spawn } = require('child_process'),
-    yarnFullPath = `${__dirname}/node_modules/.bin/yarn${process.platform == 'win32' ? '.cmd' : ''}`;
-spawn(yarnFullPath, {
-    cwd: `${__dirname}/proton`
-}).on('exit', code => {
-    if(!code){
-        console.error('Failed to build proton');
-        process.exit(1);
-    }
+    yarnFullPath = `${__dirname}/node_modules/.bin/yarn${process.platform == 'win32' ? '.cmd' : ''}`,
+    parts = [
+        'proton',
+        'react-native',
+    ];
+
+function spawnYarn(dir){
+    return new Promise((resolve, reject) => {
+        spawn(yarnFullPath, {
+            cwd: `${__dirname}/${dir}`
+        }).on('exit', code => {
+            if(code) reject(new Error(`Failed to install/build ${dir} with code ${code}`)); else resolve()
+        })
+    })
+}
+
+function partsToSentence(parts){
+    var newParts = [...parts],
+        lastPart = newParts.pop();
+    return newParts.join(', ') + ' and ' + lastPart
+}
+
+console.log('Installing needed modules for ' + partsToSentence(parts))
+
+Promise.all(parts.map(dir => spawnYarn(dir))).then(() => {
+    console.log('All done')
 })
